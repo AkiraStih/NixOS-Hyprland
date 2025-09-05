7c99d8b9cedb662a43f12c68237b990d9b004261
AkiraStih <akirastih@gmail.com>
2025-09-05 19:46:53 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -80,6 +80,7 @@
 						nixvim.homeModules.nixvim
 						nixcord.homeModules.nixcord
 						inputs.spicetify-nix.homeManagerModules.default
+						{ home.backupFileExtension = "backup"; }
 					];
 				};
 			};
